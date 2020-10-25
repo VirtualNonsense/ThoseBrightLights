7e784665a40f975cedfaba3959979dd5c77aa41a
Andreas Nachtmann <virtualnonsense@mailbox.org>
2020-10-25 11:45:24 +0100

    Added interface for everything that's drawable and updateable

---
+++ those-bright-lights/Components/IComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SE_Praktikum.Components
+{
+    /// <summary>
+    /// Derive from this class if you 
+    /// </summary>
+    public interface IComponent
+    {
+        void Draw(GameTime gameTime, SpriteBatch spriteBatch);
+
+        void Update(GameTime gameTime);
+    }
+}
