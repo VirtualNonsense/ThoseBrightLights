24a2c072e8d967e53d396eb11850e1073f837ee8
Andreas Nachtmann <virtualnonsense@mailbox.org>
2020-10-25 11:55:48 +0100

    Added simple Particle class

---
+++ those-bright-lights/Components/Sprites/Particle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SE_Praktikum.Models;
+
+namespace SE_Praktikum.Components.Sprites
+{
+    public class Particle : Sprite
+    {
+        public Vector2 Velocity;
+        
+        public Particle(Texture2D texture, IScreen Parent) : base(texture)
+        {
+            _parent = Parent;
+            Origin = new Vector2(_texture.Width / 2f, _texture.Height / 2f);
+        }
+
+        public Particle(Dictionary<string, Animation> animations, IScreen Parent) : base(animations)
+        {
+            Position += Velocity;
+            _parent = Parent;
+            if (Position.Y > (Parent.ScreenHeight + _texture.Height))
+                IsRemoveAble = true;
+        }
+    }
+}
