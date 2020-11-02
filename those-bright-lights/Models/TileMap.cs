fefd9c650d4f75f5ca0b2257c5f3c25dcc782e3f
Jonathan Wenk <wenkjo75592@th-nuernberg.de>
2020-11-02 15:45:56 +0100

    Added tilemap

---
+++ those-bright-lights/Models/TileMap.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SE_Praktikum.Models
+{
+    class TileMap
+    {
+        int rows,collums;
+
+        public Texture2D texture;
+
+        int tilewidth, tileheight;
+
+
+        public TileMap(Texture2D texture, int collums, int rows)
+        {
+            this.texture = texture;
+            this.collums = collums;
+            this.rows = rows;
+            this.tilewidth = texture.Width / collums;
+            this.tileheight = texture.Height / rows;
+
+            
+        }
+        public Rectangle Frame(int x, int y)
+        {
+            var r = new Rectangle(x * tilewidth, y * tileheight, tilewidth, tileheight);
+            return r;
+
+            //new Rectangle(_currentFrame * _animation.FrameWidth,
+            //    0,
+            //    _animation.FrameWidth,
+            //    _animation.FrameHeight);
+
+        }
+
+
+    }
+}
