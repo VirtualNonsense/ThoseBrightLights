8fe8264f559a102336a7a35e024d07719aad1db1
Jonathan Wenk <wenkjo75592@th-nuernberg.de>
2020-11-02 15:56:26 +0100

    class representation for tiled map format json

---
+++ those-bright-lights/Models/LevelBlueprint.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SE_Praktikum.Models
+{
+    class LevelBlueprint
+    {
+        int compressionlevel;
+        int height;
+        bool infinite;
+        List<LevelLayer> layers;
+        int nextlayerid;
+        int nextobjectid;
+        string orientation;
+        string renderorder;
+        string tiledversion;
+        int tileheight;
+        List<Tileset> tilesets;
+        int tilewidth;
+        string type;
+        float version;
+        int width;
+
+    }
+
+    internal class Tileset
+    {
+        int firstgid;
+        string source;
+        
+    }
+}
---
+++ those-bright-lights/Models/LevelLayer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SE_Praktikum.Models
+{
+    class LevelLayer
+    {
+        List<int> data;
+        int height;
+        int id;
+        string name;
+        float opacity;
+        string type;
+        bool visible;
+        int width;
+        int x;
+        int y;
+
+
+    }
+}
