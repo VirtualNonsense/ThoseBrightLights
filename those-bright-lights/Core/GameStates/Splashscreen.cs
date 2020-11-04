d443185ccfe39c6306114ad1b45a30948de9438a
Jonathan Wenk <wenkjo75592@th-nuernberg.de>
2020-11-04 22:26:51 +0100

    Also changed the status of the json data from "copy: never" to "copy: copy if newer" in properties

--- those-bright-lights/Core/GameStates/Splashscreen.cs
+++ those-bright-lights/Core/GameStates/Splashscreen.cs
@@ -4,11 +4,14 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Media;
 using SE_Praktikum.Models;
 using SE_Praktikum.Services.ParticleEmitter;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace SE_Praktikum.Core.GameStates
 {
     public class Splashscreen : GameState
     {
+        private TileMap _tilemap;
         private IScreen _screen;
         private readonly ExplosionEmitter _explosionEmitter;
         public Song _song;
@@ -21,10 +24,11 @@ namespace SE_Praktikum.Core.GameStates
         
         public override void LoadContent(ContentManager contentManager)
         {
+            _tilemap = new TileMap(contentManager.Load<Texture2D>("Artwork/Tilemaps/space2_4-frames"), 4, 1);
             var p = new Animation(contentManager.Load<Texture2D>("Artwork/Effects/explosion_45_45"), 7);
             _explosionEmitter.Animation = p;
             //_explosionEmitter.SpawnArea = new Rectangle(500, 100, 500, 100);
-
+            LevelBlueprint test = JsonConvert.DeserializeObject<LevelBlueprint>(File.ReadAllText(@"C:\Users\wenk-\Desktop\5.Semester\Software Engineering\those-bright-lights\those-bright-lights\Content\Level\32x32_TestMap.json"));
             _song = contentManager.Load<Song>("Audio/Music/Song3_remaster2_mp3");
             MediaPlayer.Play(_song);
             MediaPlayer.IsRepeating = true;
@@ -47,9 +51,12 @@ namespace SE_Praktikum.Core.GameStates
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
+
             spriteBatch.Begin(SpriteSortMode.FrontToBack);
             _explosionEmitter.Draw(gameTime, spriteBatch);
+           //spriteBatch.Draw(_tilemap.texture, _tilemap.Frame(1, 0), Color.White);
             spriteBatch.End();
+
         }
     }
 }
