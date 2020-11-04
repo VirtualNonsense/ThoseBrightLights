f4714264ff97b670bb31f784595fad8c507949c9
nachtmannan74843 <nachtmannan74843@th-nuernberg.de>
2020-11-04 14:08:27 +0100

    Introduced constructors for LevelLayer LevelBlueprint and TileSet

--- those-bright-lights/Models/LevelBlueprint.cs
+++ those-bright-lights/Models/LevelBlueprint.cs
@@ -10,24 +10,47 @@ namespace SE_Praktikum.Models
         public int height;
         public bool infinite;
         public List<LevelLayer> layers;
-        public int nextlayerid;
-        public int nextobjectid;
+        public int NextLayerId;
+        public int NextObjectId;
         public string orientation;
-        public string renderorder;
-        public string tiledversion;
-        public int tileheight;
-        public List<Tileset> tilesets;
-        public int tilewidth;
+        public string RenderOrder;
+        public string TiledVersion;
+        public int TileHeight;
+        public List<TileSet> TileSets;
+        public int TileWidth;
         public string type;
         public float version;
         public int width;
 
+        public LevelBlueprint(int compressionLevel, int height, bool infinite, List<LevelLayer> layers, int nextLayerId, int nextObjectId, string orientation, string renderOrder, string tiledVersion, int tileHeight, List<TileSet> tileSets, int tileWidth, string type, float version, int width)
+        {
+            compressionlevel = compressionLevel;
+            this.height = height;
+            this.infinite = infinite;
+            this.layers = layers;
+            NextLayerId = nextLayerId;
+            NextObjectId = nextObjectId;
+            this.orientation = orientation;
+            RenderOrder = renderOrder;
+            TiledVersion = tiledVersion;
+            TileHeight = tileHeight;
+            TileSets = tileSets;
+            TileWidth = tileWidth;
+            this.type = type;
+            this.version = version;
+            this.width = width;
+        }
     }
 
-    internal class Tileset
+    internal class TileSet
     {
-       public int firstgid;
+       public int FirstGId;
        public string source;
-        
+
+       public TileSet(int firstGId, string source)
+       {
+           FirstGId = firstGId;
+           this.source = source;
+       }
     }
 }
--- those-bright-lights/Models/LevelLayer.cs
+++ those-bright-lights/Models/LevelLayer.cs
@@ -17,6 +17,18 @@ namespace SE_Praktikum.Models
         public int x;
         public int y;
 
-
+        public LevelLayer(List<uint> data, int height, int id, string name, float opacity, string type, bool visible, int width, int x, int y)
+        {
+            this.data = data;
+            this.height = height;
+            this.id = id;
+            this.name = name;
+            this.opacity = opacity;
+            this.type = type;
+            this.visible = visible;
+            this.width = width;
+            this.x = x;
+            this.y = y;
+        }
     }
 }
