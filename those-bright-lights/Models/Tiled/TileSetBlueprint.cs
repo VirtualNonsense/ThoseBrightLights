fbe96a95fc58f2de7e2862a943947b8d586272ad
nachtmannan74843 <nachtmannan74843@th-nuernberg.de>
2020-11-04 16:55:16 +0100

    Refactored BlueprintClasses and moved them in separate namespace

---
+++ those-bright-lights/Models/Tiled/TileSetBlueprint.cs
@@ -0,0 +1,14 @@
+﻿namespace SE_Praktikum.Models.Tiled
+{
+    public class TileSetBlueprint
+    {
+        public int FirstGId;
+        public string Source;
+
+        public TileSetBlueprint(int firstGId, string source)
+        {
+            FirstGId = firstGId;
+            Source = source;
+        }
+    }
+}
