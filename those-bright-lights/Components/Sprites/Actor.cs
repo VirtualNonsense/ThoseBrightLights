34e7c2d023c6b4c8773b0f9ce649df622ae28cd3
pillhoferjo74678 <pillhoferjo74678@th-nuernberg.de>
2020-11-05 20:10:50 +0100

    added actor class -> base class for everything with collision & added collisionCube as Testobject

---
+++ those-bright-lights/Components/Sprites/Actor.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.ComponentModel.Design.Serialization;
+using System.Data.SqlTypes;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using NLog;
+using SE_Praktikum.Services;
+using SE_Praktikum.Components;
+using SE_Praktikum.Components.Sprites.SplashScreen;
+
+namespace SE_Praktikum.Components.Sprites
+{
+    public abstract class Actor : Sprite, ICollideAble
+    {
+
+        public bool CollisionEnabled = true;
+
+        public Actor(AnimationHandler animationHandler) : base(animationHandler)
+        {
+        }
+
+        public event EventHandler<EventArgs> OnCollide; 
+        
+
+        public abstract void BaseCollide(Actor actor);
+        
+        
+        
+        // TODO: Return Coordinate for effect Placement
+        public bool Intersects(Actor actor)
+        {
+            if (!CollisionEnabled || !actor.CollisionEnabled) return false;
+            if (Math.Abs(actor.Layer - Layer) > float.Epsilon ) return false;
+            // Calculate a matrix which transforms from A's local space into
+            // world space and then into B's local space
+            var transformAToB = Transform * Matrix.Invert(actor.Transform);
+
+            // When a point moves in A's local space, it moves in B's local space with a
+            // fixed direction and distance proportional to the movement in A.
+            // This algorithm steps through A one pixel at a time along A's X and Y axes
+            // Calculate the analogous steps in B:
+            var stepX = Vector2.TransformNormal(Vector2.UnitX, transformAToB);
+            var stepY = Vector2.TransformNormal(Vector2.UnitY, transformAToB);
+
+            // Calculate the top left corner of A in B's local space
+            // This variable will be reused to keep track of the start of each row
+            var yPosInB = Vector2.Transform(Vector2.Zero, transformAToB);
+
+            for (int yA = 0; yA < Rectangle.Height; yA++)
+            {
+                // Start at the beginning of the row
+                var posInB = yPosInB;
+
+                for (int xA = 0; xA < Rectangle.Width; xA++)
+                {
+                    // Round to the nearest pixel
+                    var xB = (int)Math.Round(posInB.X);
+                    var yB = (int)Math.Round(posInB.Y);
+
+                    if (0 <= xB && xB < actor.Rectangle.Width &&
+                        0 <= yB && yB < actor.Rectangle.Height)
+                    {
+                        // Get the colors of the overlapping pixels
+                        var colourA = TextureData[xA + yA * Rectangle.Width];
+                        var colourB = actor.TextureData[xB + yB * actor.Rectangle.Width];
+
+                        // If both pixel are not completely transparent
+                        if (colourA.A != 0 && colourB.A != 0)
+                        {
+                            return true;
+                        }
+                    }
+
+                    // Move to the next pixel in the row
+                    posInB += stepX;
+                }
+
+                // Move to the next row
+                yPosInB += stepY;
+            }
+
+            // No intersection found
+            return false;
+        }
+
+
+        protected virtual void OnOnCollide()
+        {
+            OnCollide?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
