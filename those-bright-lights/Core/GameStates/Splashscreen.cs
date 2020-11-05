53374b0d9faaa7fec10ee4c16d7ea9b30b66e421
pillhoferjo74678 <pillhoferjo74678@th-nuernberg.de>
2020-11-05 20:14:05 +0100

    added two testobjects and coded postUpdate

--- those-bright-lights/Core/GameStates/Splashscreen.cs
+++ those-bright-lights/Core/GameStates/Splashscreen.cs
@@ -12,11 +12,18 @@ namespace SE_Praktikum.Core.GameStates
         private IScreen _screen;
         private readonly ExplosionEmitter _explosionEmitter;
         public Song _song;
+        private readonly AnimationHandlerFactory _factory;
+        private CollisionCube c1;
+        private CollisionCube c2;
+        private List<Actor> _actors;
+        private Logger _logger;
 
         public Splashscreen(IScreen parent, ExplosionEmitter explosionEmitter)
         {
             _screen = parent;
             _explosionEmitter = explosionEmitter;
+            _factory = factory;
+            _logger = LogManager.GetCurrentClassLogger();
         }
         
         public override void LoadContent(ContentManager contentManager)
@@ -28,6 +35,14 @@ namespace SE_Praktikum.Core.GameStates
             _song = contentManager.Load<Song>("Audio/Music/Song3_remaster2_mp3");
             MediaPlayer.Play(_song);
             MediaPlayer.IsRepeating = true;
+            
+            var animation = new Animation(contentManager.Load<Texture2D>("Artwork/misc/testboxoderso"),1);
+            var animationSettings = new AnimationSettings();
+            
+            c1 = new CollisionCube(_factory.GetAnimationHandler(animation, animationSettings), new Input{Down = Keys.S, Left=Keys.A,Right = Keys.D,Up = Keys.W}, _screen);
+            c2 = new CollisionCube(_factory.GetAnimationHandler(animation, animationSettings), new Input{Down = Keys.Down, Left=Keys.Left,Right = Keys.Right,Up = Keys.Up}, _screen);
+            _actors = new List<Actor>(){c1,c2};      
+            c1.Position = new Vector2(200,200);
         }
 
         public override void UnloadContent()
@@ -37,18 +52,51 @@ namespace SE_Praktikum.Core.GameStates
 
         public override void Update(GameTime gameTime)
         {
-            _explosionEmitter.Update(gameTime);
+            //_explosionEmitter.Update(gameTime);
+            c1.Update(gameTime);
+            c2.Update(gameTime);
+            PostUpdate(gameTime);
+            
         }
 
-        public override void PostUpdate()
+
+        public override void PostUpdate(GameTime gameTime)
         {
+            foreach (var spriteA in _actors)
+            {
+                foreach (var spriteB in _actors)
+                {
+                    if (spriteA == spriteB)
+                        continue;
+                    if(spriteA.Intersects(spriteB))
+                    {
+                        _logger.Trace("collision");
+                        spriteB.BaseCollide(spriteA);
+                    }
+                    else
+                    {
+                        _logger.Trace("no collision");
+                    }
+                }
+            }
+            for (int i = 0; i < _actors.Count; i++)
+            {
+                if (_actors[i].IsRemoveAble)
+                {
+                    _logger.Trace("actor removed");
+                    _actors.RemoveAt(i);
+                    i--;
+                }
+            }
+                
             //throw new System.NotImplementedException();
         }
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
             spriteBatch.Begin(SpriteSortMode.FrontToBack);
-            _explosionEmitter.Draw(gameTime, spriteBatch);
+            foreach(var sprite in _actors)
+                sprite.Draw(gameTime,spriteBatch);
             spriteBatch.End();
         }
     }
