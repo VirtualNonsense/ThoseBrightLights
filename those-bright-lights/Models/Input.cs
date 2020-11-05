3671f3b5a3ef41badea8695dcafaa8cb5c7271ab
pillhoferjo74678 <pillhoferjo74678@th-nuernberg.de>
2020-11-05 20:04:34 +0100

    added basic input class

---
+++ those-bright-lights/Models/Input.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework.Input;
+
+namespace SE_Praktikum.Models
+{
+    public class Input
+    {
+        public  Keys Up { get; set; }
+
+        public Keys Down { get; set; }
+
+        public Keys Left { get; set; }
+
+        public Keys Right { get; set; }
+    }
+}
