6d83231f3c8e503c1f4233307bdbd844f571adef
nachtmannan74843 <nachtmannan74843@th-nuernberg.de>
2020-11-06 13:59:38 +0100

    preparation for further steps

--- those-bright-lights/Models/TileMap.cs
+++ those-bright-lights/Models/TileMap.cs
@@ -8,46 +8,28 @@ using System.Text;
 
 namespace SE_Praktikum.Models
 {
-    class TileMap
+    public class TileMap
     {
-        int rows,collums;
+        public readonly List<TileSet> TileSets;
 
-        public Texture2D texture;
-
-        int tilewidth, tileheight;
-
-        public int Tiles =>  collums* rows;
-
-
-        public TileMap(Texture2D texture, int collums, int rows)
+        public int Tiles
         {
-            this.texture = texture;
-            this.collums = collums;
-            this.rows = rows;
-            this.tilewidth = texture.Width / collums;
-            this.tileheight = texture.Height / rows;
-
-            
-        }
-        public Rectangle? Frame(int index)
-        {
-            if (index <= 0)
-                return null;
-            else if (index > rows * collums)
-                return null;
-            
-            int x = (index % collums) - 1;
-            int y = index / rows;
-            var r = new Rectangle(x * tilewidth, y * tileheight, tilewidth, tileheight);
-            return r;
-
-            //new Rectangle(_currentFrame * _animation.FrameWidth,
-            //    0,
-            //    _animation.FrameWidth,
-            //    _animation.FrameHeight);
-
+            get
+            {
+                var r = 0;
+                foreach (var tileSet in TileSets)
+                {
+                    r += tileSet.Tiles;
+                }
+                return r;
+            }
         }
 
+
+        public TileMap(List<TileSet> tileSets)
+        {
+            TileSets = tileSets;
+        }
 
     }
 }
