c4f865a52faf898a2a8a546d1526890c60677ddd
nachtmannan74843 <nachtmannan74843@th-nuernberg.de>
2020-11-06 13:59:04 +0100

    Introduced tileset class. it will be used for easier access to tileset info

---
+++ those-bright-lights/Models/TileSet.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace SE_Praktikum.Models
+{
+    public class TileSet
+    {
+        public int TileDimX;
+        public int TileDimY;
+        public int Columns;
+        public int Rows;
+        public Texture2D Texture;
+        public int Tiles => Columns * Rows;
+        public int StartEntry;
+        
+        public TileSet(Texture2D texture, int tileDimX, int tileDimY, int startEntry)
+        {
+            Texture = texture;
+            TileDimX = tileDimX;
+            TileDimY = tileDimY;
+            Columns = Texture.Width / TileDimX;
+            Rows = Texture.Height / TileDimY;
+            StartEntry = startEntry;
+        }
+    }
+}
