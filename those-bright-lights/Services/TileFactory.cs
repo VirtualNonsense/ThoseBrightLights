bad0935b284d28f0deac57612c17f2d0bed74632
Jonathan Wenk <wenkjo75592@th-nuernberg.de>
2020-11-06 15:50:44 +0100

    Introduced the class TileFactory, Get the frames for all indices with position handling, Impossible indices throw a warning,

---
+++ those-bright-lights/Services/TileFactory.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+using NLog;
+using SE_Praktikum.Components.Sprites;
+using SE_Praktikum.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SE_Praktikum.Services
+{
+    public class TileFactory
+    {
+        private ILogger _logger;
+        public TileFactory()
+        {
+             _logger = LogManager.GetCurrentClassLogger();
+        }
+        public Tile GenerateTile(uint index, Vector2 position, float layer, List<TileSet> tilesets)
+        {
+            foreach(var tileset in tilesets)
+            {
+                if (index >= tileset.StartEntry + tileset.Tiles)
+                    continue;
+                return new Tile(tileset.Texture, tileset.GetFrame(index), position, layer);
+
+            }
+            return null;
+
+        }
+
+        public List<Tile> GenerateTiles(List<uint> indices, float layer, List<TileSet> tilelist, int tilewidth, int tileheight, int rows, int columns)
+        {
+            if (indices.Count > rows * columns)
+            {
+                _logger.Error("Indices out of range");
+                throw new IndexOutOfRangeException();
+            }
+            List<Tile> list = new List<Tile>();
+            int row = 0;
+            int column = 0;
+            foreach(var index in indices)
+            {
+                var p = new Vector2(column * tilewidth, row * tileheight);
+                list.Add(GenerateTile(index, p, layer, tilelist));
+                column++;
+                if(column == columns)
+                {
+                    column = 0;
+                    row++;
+                }
+
+                
+            }
+
+
+
+
+            return list;
+
+
+        }
+
+    }
+}
