26e3ae49ed6036cb9631697d1b30e8320bee747e
Jonathan Wenk <wenkjo75592@th-nuernberg.de>
2020-11-06 15:37:46 +0100

    Created logger,  declared StartEntry, added logger to constructor, created GetFrame method,

--- those-bright-lights/Models/TileSet.cs
+++ those-bright-lights/Models/TileSet.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using NLog;
+using System;
 
 namespace SE_Praktikum.Models
 {
@@ -11,15 +14,34 @@ namespace SE_Praktikum.Models
         public Texture2D Texture;
         public int Tiles => Columns * Rows;
         public int StartEntry;
+        private ILogger _logger;
+
         
         public TileSet(Texture2D texture, int tileDimX, int tileDimY, int startEntry)
         {
+            _logger = LogManager.GetCurrentClassLogger();
             Texture = texture;
             TileDimX = tileDimX;
             TileDimY = tileDimY;
             Columns = Texture.Width / TileDimX;
             Rows = Texture.Height / TileDimY;
             StartEntry = startEntry;
+        }
+
+        internal Rectangle GetFrame(uint index)
+        {
+            var c = 1;
+            for (int row = 0; row < Rows; row++)
+            {
+                for (int column = 0; column < Columns; column++)
+                {
+                    if (index == c)
+                        return new Rectangle(Columns * column, Rows * row, TileDimX, TileDimY);
+                    c++;
+                }
+            }
+            _logger.Warn($"index{index} not found");
+            return Rectangle.Empty;
         }
     }
 }
