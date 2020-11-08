89bb2c7bf0679c3a7ef20aaf9926193d1fe2d927
nachtmannan74843 <nachtmannan74843@th-nuernberg.de>
2020-11-08 01:04:22 +0100

    TileModification support

--- those-bright-lights/Services/TileFactory.cs
+++ those-bright-lights/Services/TileFactory.cs
@@ -4,8 +4,8 @@ using NVorbis.Ogg;
 using SE_Praktikum.Components.Sprites;
 using SE_Praktikum.Models;
 using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Text;
 
 namespace SE_Praktikum.Services
 {
@@ -16,18 +16,20 @@ namespace SE_Praktikum.Services
         {
              _logger = LogManager.GetCurrentClassLogger();
         }
-        public Tile GenerateTile(uint index, Vector2 position, float layer, List<TileSet> tilesets, float opacity)
+        public Tile GenerateTile(uint index, Vector2 position, float layer, List<TileSet> tilesets, float opacity, int width, int height)
         {
-             
+            var t = DecodeIndex(index);
+            index = t.Item2;
             foreach(var tileset in tilesets)
             {
                 if (index > tileset.StartEntry + tileset.Tiles-1)
                     continue;
-                return new Tile(tileset.Texture, tileset.GetFrame(index), position, layer, opacity);
+                return new Tile(tileset.Texture, tileset.GetFrame(index), position, layer, opacity, width, height, t.Item1);
 
             }
-            return null;
 
+            _logger.Warn("Tile not found!");
+            throw new ArgumentOutOfRangeException();
         }
 
         public List<Tile> GenerateTiles(List<uint> indices, float layer, List<TileSet> tilelist, int tilewidth, int tileheight, int rows, int columns, float layerOpacity)
@@ -47,23 +49,53 @@ namespace SE_Praktikum.Services
                 if (index != 0)
                 {
                     var p = new Vector2(column * tilewidth, row * tileheight);
-                    list.Add(GenerateTile(index, p, layer, tilelist, layerOpacity));
+                    list.Add(GenerateTile(index, p, layer, tilelist, layerOpacity, tileheight, tilewidth));
                 }
                 column++;
-                if(column == columns)
+                if(column >= columns)
                 {
+                    if(column > columns)
+                        _logger.Warn("column > columns");
                     column = 0;
                     row++;
                 }
+                if(row > rows)
+                    _logger.Warn("row > rows");
 
                 
             }
-
-
-
-
             return list;
+        }
 
+        private (TileModifier, uint) DecodeIndex(uint codedIndex)
+        {
+            var mbits = (codedIndex >> 28);
+            uint index = mbits << 28 ^ codedIndex;
+            if (mbits == 0)
+                return (TileModifier.None, index);
+            mbits = ReveresUint(mbits<<28);
+            try
+            {
+                TileModifier mod = (TileModifier)(mbits);
+                return (mod, index);
+            }
+            catch (Exception e)
+            {
+                _logger.Error(e);
+                throw;
+            }
+        }
+
+        private uint ReveresUint(uint input)
+        {
+            var t = new BitArray(BitConverter.GetBytes(input));
+            uint output = 0;
+            for (int i = 0; i < t.Count; i++)
+            {
+                if (t[t.Count - 1 - i])
+                    output += (uint)Math.Pow(2, i);
+            }
+            return output;
 
         }
 
