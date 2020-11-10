6a7236bbffa981b9e7e2880027872bd4ad713279
pillhoferjo74678 <pillhoferjo74678@th-nuernberg.de>
2020-11-10 17:29:40 +0100

    pictures for testing

---
+++ those-bright-lights/Components/Sprites/Missile.cs
@@ -0,0 +1,17 @@
+﻿// using Microsoft.Xna.Framework.Graphics;
+// using SE_Praktikum.Services;
+//
+// namespace SE_Praktikum.Components.Sprites
+// {
+//     public class Missile : Actor
+//     {
+//         public Missile(AnimationHandler animationHandler) : base(animationHandler)
+//         {
+//         }
+//
+//         public override void BaseCollide(Actor actor)
+//         {
+//             throw new System.NotImplementedException();
+//         }
+//     }
+// }
