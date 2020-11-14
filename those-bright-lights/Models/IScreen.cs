b617b3e6c50e28a4d785eba25e287ce53cebbbe4
weinertch74668 <weinertch74668@th-nuernberg.de>
2020-11-14 17:23:48 +0100

    Put Enum in the right place - it was nonsense before Added a function to reformat windowsize "on the fly"

--- those-bright-lights/Models/IScreen.cs
+++ those-bright-lights/Models/IScreen.cs
@@ -1,15 +1,17 @@
 ﻿namespace SE_Praktikum.Models
 {
+    public enum Size
+    {
+        little = 100,
+        medium = 720,
+        big = 1000
+    }
+
     public interface IScreen
     {
-        public enum Size
-        {
-            little = 100,
-            medium = 720,
-            big = 1000
-        }
-
         int ScreenHeight { get; set; }
         int ScreenWidth { get; set; }
+
+        void SetScreenFormat(Size previous);
     }
 }
