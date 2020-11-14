29efd12990275c5e735edb7cede53d86939ecd6d
weinertch74668 <weinertch74668@th-nuernberg.de>
2020-11-14 19:27:10 +0100

    Added Rectangle class

---
+++ those-bright-lights/Components/Rectangle.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SE_Praktikum.Components
+{
+    public class Rectangle : IComponent
+    {
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
