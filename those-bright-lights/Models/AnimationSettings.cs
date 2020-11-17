42ee004b4cdd26b3975869208062ab91beecc1af
pillhoferjo74678 <pillhoferjo74678@th-nuernberg.de>
2020-11-17 10:19:38 +0100

    added second constructor to initialize updateList itself

--- those-bright-lights/Models/AnimationSettings.cs
+++ those-bright-lights/Models/AnimationSettings.cs
@@ -41,5 +41,23 @@ namespace SE_Praktikum.Models
             Opacity = opacity;
             IsPlaying = isPlaying;
         }
+        
+        public AnimationSettings(int frames = 1, float duration = 100f, float layer = 1, bool isLooping = false,  Color? color = null, 
+                                 float rotation = 0f, float scale = 1f, float opacity = 1, bool isPlaying = true)
+        {
+            Layer = layer;
+            IsLooping = isLooping;
+            UpdateList = new List<(int, float)>();
+            for (var i = 0; i < frames; i++)
+            {
+                UpdateList.Add((i,duration));
+            }     
+            SpriteEffects = SpriteEffects.None;
+            Color = color??Color.White;
+            Rotation = rotation;
+            Scale = scale;
+            Opacity = opacity;
+            IsPlaying = isPlaying;
+        }
     }
 }
