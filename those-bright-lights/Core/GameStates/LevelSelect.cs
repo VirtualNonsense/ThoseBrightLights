4bbced3d035f983103619f0ed74f018a6667ec68
nachtmannan74843 <nachtmannan74843@th-nuernberg.de>
2020-11-17 12:22:26 +0100

    added backbutton changed loading mechanic

--- those-bright-lights/Core/GameStates/LevelSelect.cs
+++ those-bright-lights/Core/GameStates/LevelSelect.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SE_Praktikum.Services.StateMachines;
 
 namespace SE_Praktikum.Core.GameStates
 {
@@ -21,7 +22,6 @@ namespace SE_Praktikum.Core.GameStates
         {
             _logger = LogManager.GetCurrentClassLogger();
             _screen = screen;
-            _buttons = new List<Menubutton>();
         }
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
@@ -36,30 +36,39 @@ namespace SE_Praktikum.Core.GameStates
 
         public override void LoadContent(ContentManager contentManager)
         {
+            if (_buttons != null) return;
+            _buttons = new List<Menubutton>();
             _logger.Debug("LoadingContent");
             var font = contentManager.Load<SpriteFont>("Font/Font2");
             var texture = contentManager.Load<Texture2D>("Artwork/Controls/button");
             _buttons.Add(new Menubutton(texture, font)
             {
                 Text = "Level 1",
-                Position = new Vector2(_screen.ScreenWidth / 2f, _screen.ScreenHeight / 3f - texture.Height),
+                Position = new Vector2(_screen.ScreenWidth / 2f, _screen.ScreenHeight / 4f - texture.Height),
                 PenColour = Color.White
             });
             _buttons.Last().Click += (sender, args) => { _logger.Debug("Level 1 selected"); };
             _buttons.Add(new Menubutton(texture, font)
             {
                 Text = "Level 2",
-                Position = new Vector2(_screen.ScreenWidth / 2f, _screen.ScreenHeight / 3f),
+                Position = new Vector2(_screen.ScreenWidth / 2f, _screen.ScreenHeight / 4f),
                 PenColour = Color.White
             });
             _buttons.Last().Click += (sender, args) => { _logger.Debug("Level 2 selected"); };
             _buttons.Add(new Menubutton(texture, font)
             {
                 Text = "Level 3",
-                Position = new Vector2(_screen.ScreenWidth / 2f, _screen.ScreenHeight / 3f + texture.Height),
+                Position = new Vector2(_screen.ScreenWidth / 2f, _screen.ScreenHeight / 4f + texture.Height),
                 PenColour = Color.White
             });
             _buttons.Last().Click += (sender, args) => { _logger.Debug("Level 3 selected"); };
+            _buttons.Add(new Menubutton(texture, font)
+            {
+                Text = "Back",
+                Position = new Vector2(_screen.ScreenWidth / 2f, _screen.ScreenHeight / 4f + 2*texture.Height),
+                PenColour = Color.White
+            });
+            _buttons.Last().Click += (sender, args) => { _logger.Debug("back"); _subject.OnNext(GameStateMachine.GameStateMachineTrigger.Back);};
         }
 
         public override void PostUpdate(GameTime gameTime)
@@ -69,7 +78,7 @@ namespace SE_Praktikum.Core.GameStates
 
         public override void UnloadContent()
         {
-            
+            _buttons = null;
         }
 
         public override void Update(GameTime gameTime)
