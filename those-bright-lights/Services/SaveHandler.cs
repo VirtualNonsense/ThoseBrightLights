a51aac205ebdf7b0264c0bf5043074f4c7edc3ea
weinertch74668 <weinertch74668@th-nuernberg.de>
2020-11-18 21:14:06 +0100

    Added Save method

--- those-bright-lights/Services/SaveHandler.cs
+++ those-bright-lights/Services/SaveHandler.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using SE_Praktikum.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace SE_Praktikum.Services
 {
-    class SaveHandler
+    public class SaveHandler
     {
+        public void Save(SaveGame saveGame)
+        {
+            using (StreamWriter sw = new StreamWriter("Savestate_1.txt"))
+            {
+                sw.WriteLine($"Level_Passed:{saveGame.clearedStage}");
+                sw.WriteLine($"Damage:{saveGame.damage}");
+                sw.WriteLine($"Player_Position:{saveGame.playerPosition}");
+                sw.WriteLine($"Enemy_Position:{saveGame.enemyPosition}");
+                sw.WriteLine($"Weapon:{saveGame.weapon}");
+            }
+
+        }
     }
 }
