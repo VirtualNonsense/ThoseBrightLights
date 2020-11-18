d4f95669b037a556b8bc72588434f77eecf5cd7f
weinertch74668 <weinertch74668@th-nuernberg.de>
2020-11-18 21:20:16 +0100

    Added Load method

--- those-bright-lights/Services/SaveHandler.cs
+++ those-bright-lights/Services/SaveHandler.cs
@@ -18,7 +18,43 @@ namespace SE_Praktikum.Services
                 sw.WriteLine($"Enemy_Position:{saveGame.enemyPosition}");
                 sw.WriteLine($"Weapon:{saveGame.weapon}");
             }
+        }
 
+        public SaveGame Load()
+        {
+            SaveGame saveGame = new SaveGame();
+
+            using (StreamReader sr = new StreamReader("CDriveDirs.txt"))
+            {
+
+                string s = sr.ReadToEnd().Trim();
+                foreach (var a in s.Split("\r\n"))
+                {
+                    var keyValuePair = a.Split(':');
+                    switch (keyValuePair[0])
+                    {
+                        case "Level_Passed":
+                            saveGame.clearedStage = int.Parse(keyValuePair[1]);
+                            break;
+                        case "Damage":
+                            saveGame.damage = int.Parse(keyValuePair[1]);
+                            break;
+                        case "Player_Position":
+                            saveGame.playerPosition = int.Parse(keyValuePair[1]);
+                            break;
+                        case "Enemy_Position":
+                            saveGame.enemyPosition = int.Parse(keyValuePair[1]);
+                            break;
+                        case "Weapon":
+                            saveGame.weapon = int.Parse(keyValuePair[1]);
+                            break;
+                        default:
+                            Console.WriteLine("What??");
+                            break;
+                    }
+                }
+            }
+            return saveGame;
         }
     }
 }
