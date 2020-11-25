ea3a8e68056d7cf9910c0a649cf132a022d1e464
Jonathan Wenk <wenkjo75592@th-nuernberg.de>
2020-11-25 11:05:42 +0100

    Bullet class for collision (like Actor)

---
+++ those-bright-lights/Components/Sprites/Weapons/Bullet.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SE_Praktikum.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SE_Praktikum.Components.Sprites.Weapons
+{
+    public class Bullet : Actor
+    {
+        public Bullet(AnimationHandler animationHandler) : base(animationHandler)
+        {
+        }
+
+        public override void BaseCollide(Actor actor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            base.Draw(gameTime, spriteBatch);
+        }
+
+       
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void OnOnCollide()
+        {
+            base.OnOnCollide();
+        }
+    }
+}
