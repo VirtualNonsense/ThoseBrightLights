ce71e65e0dd92738d737f57fe8e7bf8e5e5da6d5
Jonathan Wenk <wenkjo75592@th-nuernberg.de>
2020-11-25 11:09:31 +0100

    Updated the Draw and Update method for each components. Notify method to trigger shooting event

--- those-bright-lights/Core/Level.cs
+++ those-bright-lights/Core/Level.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using SE_Praktikum.Components;
 using SE_Praktikum.Components.Sprites;
+using SE_Praktikum.Models;
 
 namespace SE_Praktikum.Core
 {
@@ -20,12 +21,40 @@ namespace SE_Praktikum.Core
 
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            throw new System.NotImplementedException();
+            foreach(var i in _components)
+            {
+                i.Draw(gameTime, spriteBatch);
+            }
         }
 
         public void Update(GameTime gameTime)
         {
-            throw new System.NotImplementedException();
+            foreach(var i in _components)
+            {
+                i.Update(gameTime);
+            }
+            foreach(ICollideAble i in _components)
+            {
+                foreach(ICollideAble j in _components)
+                {
+                    if(i.Collides(j))
+                    {
+
+                    }
+                    
+                }
+            }
+        }
+
+        
+        public void OnNotify(LevelEvent levelEvent)
+        {
+            var t = (LevelEvent.ShootBullet)levelEvent;
+            //if player or enemy shoots the ShootBullet event triggers
+            if (t!=null)                                    
+            {
+                _components.Add(t.Bullet);
+            }
         }
     }
 }
---
+++ those-bright-lights/Models/LevelEvent.cs
@@ -0,0 +1,22 @@
+﻿using SE_Praktikum.Components.Sprites.Weapons;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SE_Praktikum.Models
+{
+    public class LevelEvent : EventArgs
+    {
+        public class GameQuit : LevelEvent
+        {
+
+        }
+
+        public class ShootBullet : LevelEvent
+        {
+            public Bullet Bullet;
+        }
+
+
+    }
+}
