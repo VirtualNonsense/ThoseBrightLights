7f15134077b612a828a1ca0a95a109a7e9c1e27e
pillhoferjo74678 <pillhoferjo74678@th-nuernberg.de>
2020-11-28 13:58:21 +0100

    fixed update method in Level, current version of .netCore doesn't support implizit type casting in foreach loops, so we need to use ofType (works though)

--- those-bright-lights/Core/Level.cs
+++ those-bright-lights/Core/Level.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using Newtonsoft.Json;
+using NLog;
 using SE_Praktikum.Components;
 using SE_Praktikum.Components.Sprites;
 using SE_Praktikum.Models;
+using SE_Praktikum.Models.Tiled;
+using SE_Praktikum.Services.Factories;
 
 namespace SE_Praktikum.Core
 {
@@ -12,14 +19,16 @@ namespace SE_Praktikum.Core
         private readonly MapFactory _mapFactory;
         private readonly PlayerFactory _playerFactory;
         private List<IComponent> _components;
+        private Logger _logger;
 
-        
+
         //Constructor
         public Level(MapFactory mapFactory, PlayerFactory playerFactory)
         {
             _mapFactory = mapFactory;
             _playerFactory = playerFactory;
             _components = new List<IComponent>();
+            _logger = LogManager.GetCurrentClassLogger();
         }
 
 
@@ -37,15 +46,15 @@ namespace SE_Praktikum.Core
             {
                 i.Update(gameTime);
             }
-            foreach(ICollideAble i in _components)
+            foreach(var actor1 in _components.OfType<Actor>())
             {
-                foreach(ICollideAble j in _components)
+                foreach(var actor2 in _components.OfType<Actor>())
                 {
-                    if(i.Collides(j))
+                    var collisionPosition = actor1.Intersects(actor2);
+                    if(!(collisionPosition is null))
                     {
-
+                        _logger.Info("Collision");
                     }
-                    
                 }
             }
         }
