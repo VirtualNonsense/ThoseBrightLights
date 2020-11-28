54f5a8728567a5f5362d9e393b680133332e69d7
pillhoferjo74678 <pillhoferjo74678@th-nuernberg.de>
2020-11-28 13:50:58 +0100

    created inputfactory, still need consideration about multiple inputs

---
+++ those-bright-lights/Services/Factories/InputFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Xna.Framework.Input;
+using SE_Praktikum.Models;
+
+namespace SE_Praktikum.Services.Factories
+{
+    public class InputFactory
+    {
+        public InputFactory()
+        {
+            
+        }
+
+        public Input GetInstance()
+        {
+            var i = new Input(Keys.W,Keys.S, Keys.A, Keys.D, Keys.Q, Keys.E);
+            //TODO Andi du weißt scho was hier zu tun is
+            return i;
+        }
+
+    }
+}
