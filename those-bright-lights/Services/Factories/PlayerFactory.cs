9d766ce95bf72cb7d1a0adedbb09a7c1ad2a8cfc
pillhoferjo74678 <pillhoferjo74678@th-nuernberg.de>
2020-11-28 13:51:10 +0100

    created playerfactory

---
+++ those-bright-lights/Services/Factories/PlayerFactory.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection.Metadata;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using SE_Praktikum.Components.Sprites;
+using SE_Praktikum.Models;
+
+namespace SE_Praktikum.Services.Factories
+{
+    public class PlayerFactory
+    {
+        private readonly AnimationHandlerFactory _animationHandlerFactory;
+        private readonly InputFactory _inputFactory;
+
+        public PlayerFactory(AnimationHandlerFactory animationHandlerFactory, InputFactory inputFactory)
+        {
+            _animationHandlerFactory = animationHandlerFactory;
+            _inputFactory = inputFactory;
+        }
+
+        public Player GetInstance(ContentManager contentManager)
+        {
+            var texture2D = contentManager.Load<Texture2D>("Artwork/Actors/spaceship");
+            var tileSet = new TileSet(texture2D);
+            var animationSettings = new AnimationSettings(1,isPlaying:false);
+            var input = _inputFactory.GetInstance();
+            var p = new Player(_animationHandlerFactory.GetAnimationHandler(tileSet,animationSettings),input);
+
+            return p;
+        } 
+    }
+}
