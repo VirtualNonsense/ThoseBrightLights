f2566c808ab9a1a3f65740586eb64bce73775a33
nachtmannan74843 <nachtmannan74843@th-nuernberg.de>
2020-11-30 20:35:02 +0100

    Bugfix: fixed splashscreentimeout cleanup

--- those-bright-lights/Core/GameStates/Splashscreen.cs
+++ those-bright-lights/Core/GameStates/Splashscreen.cs
@@ -1,49 +1,28 @@
-﻿using System.Collections.Generic;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
 using NLog;
-using SE_Praktikum.Components.Sprites;
-using SE_Praktikum.Components.Sprites.SplashScreen;
 using SE_Praktikum.Models;
-using SE_Praktikum.Services;
-using SE_Praktikum.Services.ParticleEmitter;
 using SE_Praktikum.Services.StateMachines;
-using SE_Praktikum.Components;
-using SE_Praktikum.Services.Factories;
-using Newtonsoft.Json;
-using SE_Praktikum.Models.Tiled;
-using System.IO;
 
 namespace SE_Praktikum.Core.GameStates
 {
     public class Splashscreen : GameState
     {
-        private IScreen _screen;
-        private int _splashscreenTime = 60;
-        private readonly ExplosionEmitter _explosionEmitter;
-        private Map TestMap;
-        public Song _song;
         private Logger _logger;
-        private MapFactory MapFactory;
+        private IScreen _screen;
+        private int _elapsedTime = 0;
+        private int _splashscreenTime = 60;
 
-        public Splashscreen(IScreen parent, ExplosionEmitter explosionEmitter, MapFactory mapFactory)
+        public Splashscreen(IScreen parent)
         {
             _logger = LogManager.GetCurrentClassLogger();
             _screen = parent;
-            _explosionEmitter = explosionEmitter;
-            MapFactory = mapFactory;
         }
         
         public override void LoadContent(ContentManager contentManager)
         {
-            var p = new TileSet(contentManager.Load<Texture2D>("Artwork/Effects/explosion_45_45"), 45, 45,0);
-            _explosionEmitter.TileSet = p;
-            //_explosionEmitter.SpawnArea = new Rectangle(500, 100, 500, 100);
-            var LevelBlueprint = JsonConvert.DeserializeObject<LevelBlueprint>(File.ReadAllText(@".\Content\Level\TestLevel\TestLevel.json"));
-            TestMap = MapFactory.LoadMap(contentManager, LevelBlueprint);
 
         }
 
@@ -54,8 +33,8 @@ namespace SE_Praktikum.Core.GameStates
 
         public override void Update(GameTime gameTime)
         {
-            _explosionEmitter.Update(gameTime);
-            if(Keyboard.GetState().IsKeyDown(Keys.Space) || _splashscreenTime < gameTime.ElapsedGameTime.Seconds)
+            _elapsedTime += gameTime.ElapsedGameTime.Seconds;
+            if(Keyboard.GetState().IsKeyDown(Keys.Escape) || _splashscreenTime < _elapsedTime)
             {
                 _logger.Debug("exiting splashscreen");
                 _subject.OnNext(GameStateMachine.GameStateMachineTrigger.SkipSplashScreen);
@@ -76,8 +55,6 @@ namespace SE_Praktikum.Core.GameStates
                               null,
                               RasterizerState.CullCounterClockwise, // Render only the texture side that faces the camara to boost performance 
                               _screen.Camera.GetCameraEffect());
-            TestMap.Draw(gameTime, spriteBatch);
-            _explosionEmitter.Draw(gameTime, spriteBatch);
             spriteBatch.End();
         }
     }
