2f9bd802574c9a183db8f1b309fa2d1026cc7260
nachtmannan74843 <nachtmannan74843@th-nuernberg.de>
2020-12-01 17:15:33 +0100

    added slider

---
+++ those-bright-lights/Components/Controls/Slider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using SE_Praktikum.Core;
+using SE_Praktikum.Services;
+
+namespace SE_Praktikum.Components.Controls
+{
+    public class Slider : MenuItem
+    {
+        public Slider(List<AnimationHandler> handler, Camera camera, bool useCenterAsOrigin) : base(handler, camera, useCenterAsOrigin)
+        {
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
