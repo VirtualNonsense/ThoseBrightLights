6c7873f7c8af5ce0d2b80cf1bbc8096a34fa75b9
pillhoferjo74678 <pillhoferjo74678@th-nuernberg.de>
2020-12-02 12:53:44 +0100

    added weapon and bullet class for laser

---
+++ those-bright-lights/Components/Sprites/Weapons/Laser.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SE_Praktikum.Services;
+
+namespace SE_Praktikum.Components.Sprites.Weapons
+{
+    public class Laser : Bullet
+    {
+        private readonly Vector2 _spaceShipVelocity;
+        private float _elapsedTime = 0;
+
+        public Laser(AnimationHandler animationHandler, Particle explosion) : base(animationHandler, explosion)
+        {
+            Velocity = new Vector2(2,0);
+        }
+        
+        public override void Update(GameTime gameTime)
+        {
+            _elapsedTime += gameTime.ElapsedGameTime.Milliseconds / 1000f;
+            Position += _spaceShipVelocity+Velocity;
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            base.Draw(gameTime, spriteBatch);
+        }
+
+        protected override void InvokeOnCollide()
+        {
+            base.InvokeOnCollide();
+        }
+    }
+}
