24e73a6f60985b55f412debb71369d60d8a9c1cf
pillhoferjo74678 <pillhoferjo74678@th-nuernberg.de>
2020-12-03 10:25:40 +0100

    fixed motion equation

--- those-bright-lights/Components/Sprites/Weapons/Bullet.cs
+++ those-bright-lights/Components/Sprites/Weapons/Bullet.cs
@@ -4,28 +4,29 @@ using SE_Praktikum.Services;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using NLog;
 using SE_Praktikum.Models;
 
 namespace SE_Praktikum.Components.Sprites.Weapons
 {
     public class Bullet : Actor
     {
-        public Particle Explosion;
+        public readonly Particle Explosion;
         protected Vector2 Velocity;
         protected Vector2 Acceleration;
-        protected Vector2 BulletPosition;
-        public Bullet(AnimationHandler animationHandler, Particle explosion) : base(animationHandler)
+
+        protected Bullet(AnimationHandler animationHandler, Particle explosion) : base(animationHandler)
         {
             Explosion = explosion;
             Velocity = Vector2.Zero;
             Acceleration = Vector2.Zero;
         }
 
-        public Vector2 Movement(Vector2 spaceshipVelocity,
-            float elapsedGameTime)
+        protected Vector2 Movement(Vector2 spaceshipVelocity, float elapsedTime)
         {
-            Vector2 position = spaceshipVelocity * elapsedGameTime +
-                               0.5f * Acceleration * elapsedGameTime * elapsedGameTime + Velocity * elapsedGameTime + Position;
+            LogManager.GetCurrentClassLogger().Info(elapsedTime);
+            Vector2 position = spaceshipVelocity+
+                               0.5f * Acceleration * elapsedTime + Velocity + Position;
             return position;
         }
         
