c191d06337a2a59333994fd3ff5a969615999898
nachtmannan74843 <nachtmannan74843@th-nuernberg.de>
2020-12-05 15:30:40 +0100

    easier access to toplayer (collision layer)

--- those-bright-lights/Components/Map.cs
+++ those-bright-lights/Components/Map.cs
@@ -7,24 +7,31 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using SE_Praktikum.Components.Sprites;
 using System.Data;
+using System.Linq;
 using SE_Praktikum.Models.Tiled;
 
 namespace SE_Praktikum.Components
 {
     public class Map: IComponent
     {
-        private List<Tile> _tile;
+        private List<Tile> _tiles;
+        
+        public float TopLayer { get; }
 
 
-        public Map (List<Tile> tiles)
+        public Map (List<Tile> tileses)
         {
-            _tile = tiles;
-
+            _tiles = tileses;
+            TopLayer = 0;
+            foreach (var tile in _tiles.Where(tile => TopLayer < tile.Layer))
+            {
+                TopLayer = tile.Layer;
+            }
         }
 
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            foreach(var tile in _tile)
+            foreach(var tile in _tiles)
             {
                 tile.Draw(gameTime, spriteBatch);
             }
