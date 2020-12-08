60631f472b98ec66d475f657f3527a443bb8b2f7
pillhoferjo74678 <pillhoferjo74678@th-nuernberg.de>
2020-12-08 12:45:11 +0100

    new property _points and method getedges

--- those-bright-lights/Models/Polygon.cs
+++ those-bright-lights/Models/Polygon.cs
@@ -11,13 +11,28 @@ namespace SE_Praktikum.Models
 {
     public class Polygon
     {
+        public Polygon(List<Vector2> points)
+        {
+            _points = points;
+        }
+
         public float Rotation { get; set; }
 
         public Vector2 Position { get; set; }
 
         public Vector2 Origin { get; set; }
 
-        
+        private List<Vector2> _points { get; }
+
+        private List<Vector2> GetEdges()
+        {
+            var l = new List<Vector2>();
+            for (var i = 0; i < _points.Count; i++)
+            {
+                //calculate next i modulo length of points to also get the edge from last to first point
+                var nextI = i % _points.Count;
+                l.Add(new Vector2(_points[nextI].X - _points[i].X, _points[new Index()].Y - _points[i].Y));
+            }
 
 
 
