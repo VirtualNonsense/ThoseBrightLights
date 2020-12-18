91ee9776bab5e19c42f3813b657c13f84706e395
Jonathan Wenk <wenkjo75592@th-nuernberg.de>
2020-12-18 16:58:55 +0100

    Subclass for collision-json file

---
+++ those-bright-lights/Models/Tiled/ObjectGroupBluePrint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SE_Praktikum.Models.Tiled
+{
+    class ObjectGroupBluePrint
+    {
+        public string draworder;
+        public int id;
+        public string name;
+        public int opacity;
+        public string type;
+        public bool visible;
+        public float x;
+        public float y;
+
+
+        
+
+    }
+}
