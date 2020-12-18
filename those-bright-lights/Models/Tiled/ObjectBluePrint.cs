cd7030c712dbb6aef59b9350aae1c7aec3863268
Jonathan Wenk <wenkjo75592@th-nuernberg.de>
2020-12-18 16:57:09 +0100

    Subclass for polygon data from collision json file

---
+++ those-bright-lights/Models/Tiled/ObjectBluePrint.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SE_Praktikum.Models.Tiled
+{
+    class ObjectBluePrint
+    {
+        public float height;
+        public int id;
+        public string name;
+        public float rotation;
+        public string type;
+        public bool visible;
+        public float width;
+        public float x;
+        public float y;
+        public Vector2[] polygon;
+
+    }
+}
