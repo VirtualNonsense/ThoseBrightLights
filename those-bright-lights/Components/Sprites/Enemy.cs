4760bf96579d7866ba4347ac49475a371c31160b
nachtmannan74843 <nachtmannan74843@th-nuernberg.de>
2020-12-20 03:09:18 +0100

    Updating Hitbox correctly

--- those-bright-lights/Components/Sprites/Enemy.cs
+++ those-bright-lights/Components/Sprites/Enemy.cs
@@ -21,22 +21,24 @@ namespace SE_Praktikum.Components.Sprites
         private bool _canShoot;
         private InterAction _i;
 
-
+        private bool _hitBoxFlipped = false;
         public override float Rotation
         {
             get => base.Rotation;
             set
             {
-                if (Math.Abs(MathExtensions.RadToDeg(value)) > 90 && !FlippedHorizontal)
-                {
-                    HitBox.MirrorHorizontal(Position);
-                }
-                else if (Math.Abs(MathExtensions.RadToDeg(value)) < 90 && FlippedHorizontal)
-                {
-                    HitBox.MirrorHorizontal(Position);
-                }
-
                 base.Rotation = value;
+                if (Rotation < 3 * Math.PI/2 && Rotation > Math.PI/2)
+                {
+                    if (_hitBoxFlipped) return;
+                    ViewBox = ViewBox.MirrorSingleVertical(Position);
+                    _hitBoxFlipped = true;
+                }
+                else if (_hitBoxFlipped)
+                {
+                    ViewBox = ViewBox.MirrorSingleVertical(Position);
+                    _hitBoxFlipped = false;
+                }
             }
         }
 
@@ -58,11 +60,11 @@ namespace SE_Praktikum.Components.Sprites
                 _timeSinceLastShot = 0;
             }
             Vector2 velocity = Vector2.Zero;
-            
-            base.Update(gameTime);
+
             ViewBox.Position = Position;
             ViewBox.Rotation = Rotation;
             ViewBox.Layer = Layer;
+            base.Update(gameTime);
         }
 
         
