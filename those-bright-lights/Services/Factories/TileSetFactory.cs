3e5fd1381a44de860158a65cbce43cfaebcc3773
Jonathan Wenk <wenkjo75592@th-nuernberg.de>
2020-12-20 17:10:00 +0100

    Method for creating a tileset, ready to use for map factory

--- those-bright-lights/Services/Factories/TileSetFactory.cs
+++ those-bright-lights/Services/Factories/TileSetFactory.cs
@@ -6,6 +6,8 @@ using Newtonsoft.Json;
 using NLog;
 using SE_Praktikum.Models;
 using SE_Praktikum.Models.Tiled;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 
 namespace SE_Praktikum.Services.Factories
 {
@@ -23,10 +25,44 @@ namespace SE_Praktikum.Services.Factories
             _logger = LogManager.GetCurrentClassLogger();
         }
 
-        public TileSet GetInstance(string jsonpath)
+        public TileSet GetInstance(string jsonpath, int startindex)
         {
             var TileSet = JsonConvert.DeserializeObject<TileSetBlueprint>(File.ReadAllText(jsonpath));
             var Texture = _contentManager.Load<Texture2D>(TileSet.image);
+            var Dictionary = new Dictionary<int, Polygon[]>();
+            
+            foreach(var tile in TileSet.tiles)
+            {
+                var count = 0;
+                foreach(var objectt in tile.objectgroup.objects)
+                {
+                    if (objectt.polygon != null)
+                        continue;
+                    var polygon = new Polygon(
+                        Vector2.Zero,
+                        new Vector2(objectt.width/2,objectt.height/2),
+                        0,
+                        new List<Vector2> 
+                        {
+                            new Vector2(0,0),
+                            new Vector2(objectt.width,0),
+                            new Vector2(objectt.width,objectt.height),
+                            new Vector2(0,objectt.height)
+                        }
+                        );
+                    if(!(Dictionary.ContainsKey(objectt.id)))
+                    {
+                        Dictionary.Add(objectt.id, new Polygon[tile.objectgroup.objects.Length]);
+
+                    }
+                    Dictionary[objectt.id][count] = polygon;
+                    count++;
+                    
+
+                }
+                
+            }
+            return new TileSet(Texture, TileSet.tilewidth,TileSet.tileheight,Dictionary,startindex);
 
         }
     }
