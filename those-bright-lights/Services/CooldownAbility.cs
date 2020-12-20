a9d09501a207a4284154c093aae315e1830af89c
pillhoferjo74678 <pillhoferjo74678@th-nuernberg.de>
2020-12-20 14:57:56 +0100

    added class cooldownAbility to simplify update methods

---
+++ those-bright-lights/Services/CooldownAbility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace SE_Praktikum.Services
+{
+    public class CooldownAbility
+    {
+        private int _currentTimer;
+        private readonly int _cooldown;
+
+        public CooldownAbility(int cooldown, Action ability)
+        {
+            _currentTimer = cooldown;
+            _cooldown = cooldown;
+            _ability = ability;
+        }
+
+        private readonly Action _ability;
+
+        public void Update(GameTime gameTime)
+        {
+            _currentTimer += (int)gameTime.ElapsedGameTime.TotalMilliseconds;
+        }
+
+        public void Fire()
+        {
+            if (_currentTimer < _cooldown) return;
+            _currentTimer = 0;
+            _ability();
+        }
+    }
+}
