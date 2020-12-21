b051094e50023b399ffdc86dd6a1bd420095577b
Jonathan Wenk <wenkjo75592@th-nuernberg.de>
2020-12-21 16:23:55 +0100

    constructor update

--- those-bright-lights/Models/Tiled/TileSetBlueprint.cs
+++ those-bright-lights/Models/Tiled/TileSetBlueprint.cs
@@ -22,7 +22,7 @@ namespace SE_Praktikum.Models.Tiled
         public TileBluePrint[] tiles;
         
 
-        public TileSetBlueprint(int columns, string image, int imageheight, int imagewidth, int margin, string name, int spacing, int tilecount, string tiledversion, int tileheight, int tilewidth, string type, float version)
+        public TileSetBlueprint(int columns, string image, int imageheight, int imagewidth, int margin, string name, int spacing, int tilecount, string tiledversion, int tileheight, int tilewidth, string type, float version, TileBluePrint []tiles)
         {
             this.columns = columns;
             this.image = image;
@@ -37,6 +37,7 @@ namespace SE_Praktikum.Models.Tiled
             this.tilewidth = tilewidth;
             this.type = type;
             this.version = version;
+            this.tiles = tiles;
         }
 
 
