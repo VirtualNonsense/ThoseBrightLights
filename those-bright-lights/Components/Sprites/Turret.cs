d368a39544952bd2fa1a852f4499e69016399046
pillhoferjo74678 <pillhoferjo74678@th-nuernberg.de>
2020-12-23 13:14:09 +0100

    added turretclass

---
+++ those-bright-lights/Components/Sprites/Turret.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using NLog;
+using SE_Praktikum.Extensions;
+using SE_Praktikum.Services;
+
+namespace SE_Praktikum.Components.Sprites
+{
+    public class Turret : Enemy
+    {
+        private Logger _logger;
+        public Turret(AnimationHandler animationHandler, float speed = 3, float health = 50, SoundEffect impactSound = null) : base(animationHandler, speed, health, impactSound)
+        {
+            _logger = LogManager.GetCurrentClassLogger();
+            RotateAndShoot = false;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            Shoot.Update(gameTime);
+            if (I == InterAction.InView && Target != null)
+            {
+                _logger.Info(Rotation);
+                Shoot.Fire();
+            }
+            base.Update(gameTime);
+        }
+    }
+}
--- those-bright-lights/Services/Factories/EnemyFactory.cs
+++ those-bright-lights/Services/Factories/EnemyFactory.cs
@@ -37,6 +37,24 @@ namespace SE_Praktikum.Services.Factories
             e.AddWeapon(_weaponFactory.GetLasergun(contentManager));
 
             return e;
-        } 
+        }
+
+        public Turret GetTurret(ContentManager contentManager)
+        {
+            SoundEffect impactSound = contentManager.Load<SoundEffect>("Audio/Sound_Effects/Collusion/ClinkBell");
+            var tileSet = _tileSetFactory.GetInstance(@".\Content\MetaData\TileSets\alien_ship.json",0);
+            var animationSettings = new AnimationSettings(1,isPlaying:false);
+            var e = new Turret(_animationHandlerFactory.GetAnimationHandler(tileSet,animationSettings), impactSound:impactSound);
+            e.Position = new Vector2(100,50);
+            e.ViewBox = new Polygon(Vector2.Zero, Vector2.Zero, 0, new List<Vector2>
+            {
+                new Vector2(0,0),
+                new Vector2(300,-100),
+                new Vector2(300,100),
+            });
+            e.AddWeapon(_weaponFactory.GetLasergun(contentManager));
+
+            return e;
+        }
     }
 }
