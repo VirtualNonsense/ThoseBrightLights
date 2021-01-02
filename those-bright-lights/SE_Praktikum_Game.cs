5f9298ffe9cdc13e13a6f286caa8ed159009a1f5
weinertch74668 <weinertch74668@th-nuernberg.de>
2021-01-02 17:13:05 +0100

    Added TODO Reminder for adding Setting stuff

--- those-bright-lights/SE_Praktikum_Game.cs
+++ those-bright-lights/SE_Praktikum_Game.cs
@@ -226,6 +226,7 @@ namespace SE_Praktikum
         private void AdaptsSettings()
         {
             MediaPlayer.Volume = SaveGame.musicVolume;
+            // TODO: If other Settings are saveable continue to add them HERE!
         }
     }
 }
