6a865c901e96c4cce9a50ad342f0972e79513c50
weinertch74668 <weinertch74668@th-nuernberg.de>
2021-01-02 14:37:41 +0100

    Added ISaveGameHandler

---
+++ those-bright-lights/Core/ISaveGameHandler.cs
@@ -0,0 +1,21 @@
+﻿using SE_Praktikum.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SE_Praktikum.Core
+{
+    enum SaveSlot
+    {
+        Slot1,
+        Slot2,
+        Slot3
+    }
+    interface ISaveGameHandler
+    {
+        void Save();
+        void Load();
+
+        SaveSlot saveSlot { get; set; }
+    }
+}
