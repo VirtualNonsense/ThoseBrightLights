f65ee8c448f382c8fb0729322d6b52b48b61a330
Jonathan Wenk <wenkjo75592@th-nuernberg.de>
2021-01-04 18:10:35 +0100

    New methods for tileset placing in map

--- those-bright-lights/Services/Factories/PowerUpFactory.cs
+++ those-bright-lights/Services/Factories/PowerUpFactory.cs
@@ -16,74 +16,77 @@ namespace SE_Praktikum.Services.Factories
     {
         public Logger _logger;
         private AnimationHandlerFactory _animationHandlerFactory;
+        private readonly WeaponFactory _weaponFactory;
         private readonly ParticleFactory _particleFactory;
         private readonly TileSetFactory _tileSetFactory;
         private readonly ContentManager _contentManager;
+        
 
-        public PowerUpFactory(AnimationHandlerFactory animationHandlerFactory, ParticleFactory particleFactory, TileSetFactory tileSetFactory,ContentManager contentManager)
+        public PowerUpFactory(AnimationHandlerFactory animationHandlerFactory,WeaponFactory weaponFactory, ParticleFactory particleFactory, TileSetFactory tileSetFactory,ContentManager contentManager)
         {
             _animationHandlerFactory = animationHandlerFactory;
+            this._weaponFactory = weaponFactory;
             _particleFactory = particleFactory;
             _tileSetFactory = tileSetFactory;
             _contentManager = contentManager;
         }
 
-        public HealthPowerUp HealthGetInstance(float health)
+        public HealthPowerUp HealthGetInstance(float health, Vector2? position = null, float layer = 0)
         {
             var tileSet = _tileSetFactory.GetInstance(@".\Content\MetaData\TileSets\PowerUpTry1.json", 0);
             var animationSettings = new AnimationSettings(new List<(int,float)> {(0,1)}, isPlaying: false);
             var hp = new HealthPowerUp(_animationHandlerFactory.GetAnimationHandler(tileSet, animationSettings), healthbonus:health);
+            hp.Layer = layer;
+            hp.Position = position ?? new Vector2(0, 0);
             return hp;
         }
 
-        public InstaDeathPowerUp DeathGetInstance()
+        public InstaDeathPowerUp DeathGetInstance(Vector2? position = null, float layer = 0)
         {
             var tileSet = _tileSetFactory.GetInstance(@".\Content\MetaData\TileSets\PowerUpTry1.json", 0);
             var animationSettings = new AnimationSettings(1, isPlaying: false);
             var ikp = new InstaDeathPowerUp(_animationHandlerFactory.GetAnimationHandler(tileSet, animationSettings));
+            ikp.Layer = layer;
+            ikp.Position = position ?? new Vector2(0, 0);
             return ikp;
         }
 
-        public LaserAmmoPowerUp LAmmoGetInstance()
+        public WeaponPowerUp RocketGetInstance(Vector2? position = null, float layer = 0)
         {
             var tileSet = _tileSetFactory.GetInstance(@".\Content\MetaData\TileSets\PowerUpTry1.json", 0);
-            var animationSettings = new AnimationSettings(1, isPlaying: false);
-            var la = new LaserAmmoPowerUp(_animationHandlerFactory.GetAnimationHandler(tileSet, animationSettings));
-            return la;
+            var animationSettings = new AnimationSettings(new List<(int, float)> { (11,1) }, isPlaying: false);
+            var rp = new WeaponPowerUp(_animationHandlerFactory.GetAnimationHandler(tileSet, animationSettings),_weaponFactory.GetMissileLauncher(_contentManager));
+            rp.Layer = layer;
+            rp.Position = position ?? new Vector2(0, 0);
+            return rp;
         }
 
-        public RocketAmmoPowerUp RAmmoGetInstance()
+        public WeaponPowerUp LaserGetInstance(Vector2? position = null, float layer = 0)
         {
             var tileSet = _tileSetFactory.GetInstance(@".\Content\MetaData\TileSets\PowerUpTry1.json", 0);
-            var animationSettings = new AnimationSettings(1, isPlaying: false);
-            var ra = new RocketAmmoPowerUp(_animationHandlerFactory.GetAnimationHandler(tileSet, animationSettings));
-            return ra;
+            var animationSettings = new AnimationSettings(new List<(int, float)> { (22, 1) }, isPlaying: false);
+            var lp = new WeaponPowerUp(_animationHandlerFactory.GetAnimationHandler(tileSet, animationSettings), _weaponFactory.EnemyGetLasergun(_contentManager));
+            lp.Layer = layer;
+            lp.Position = position ?? new Vector2(0, 0);
+            return lp;
         }
 
-        //public ShotgunAmmoPowerUp SAmmoGetInstance()
-        //{
 
-        //}
 
-        //public RocketPowerUp RocketGetInstance()
-        //{
 
-        //}
 
-        //public ShotgunPowerUp ShotgunGetInstance()
-        //{
 
-        //}
+
 
         //public ScoreBonusPowerUp ScoreGetInstance()
         //{
 
         //}
 
-        
 
-        
 
-        
+
+
+
     }
 }
