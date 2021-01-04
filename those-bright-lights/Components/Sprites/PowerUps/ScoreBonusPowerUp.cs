3eedc4aa66b859f1ffc7ce86439e3b9226e65aac
Jonathan Wenk <wenkjo75592@th-nuernberg.de>
2021-01-04 17:24:30 +0100

    Subclasses for each powerup

---
+++ those-bright-lights/Components/Sprites/PowerUps/ScoreBonusPowerUp.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework.Audio;
+using SE_Praktikum.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SE_Praktikum.Components.Sprites.PowerUps
+{
+    public class ScoreBonusPowerUp : PowerUp
+    {
+        public ScoreBonusPowerUp(AnimationHandler animationHandler, float health = 0.01f, SoundEffect soundEffect = null) : base(animationHandler, health, soundEffect)
+        {
+
+        }
+    }
+}
