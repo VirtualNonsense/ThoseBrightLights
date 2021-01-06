843195bec992357fec1f496ad4270cff00a764cc
pillhoferjo74678 <pillhoferjo74678@th-nuernberg.de>
2021-01-06 14:15:59 +0100

    player collides with enemy

--- those-bright-lights/Components/Sprites/Player.cs
+++ those-bright-lights/Components/Sprites/Player.cs
@@ -81,5 +81,18 @@ namespace SE_Praktikum.Components.Sprites
             Health -= damage;
             base.InvokeOnTakeDamage(damage);
         }
+
+        protected override void ExecuteInteraction(Actor other)
+        {
+            switch (other)
+            {
+                case Enemy e:
+                    var v = _impactPolygon.Position - e.Position;
+                    v /= v.Length();
+                    Position += v;
+                    break;
+            }
+            base.ExecuteInteraction(other);
+        }
     }
 }
