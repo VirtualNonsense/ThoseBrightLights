dda1fe40ccd15e8e18f30eb739c5fd1a28132832
nachtmannan74843 <nachtmannan74843@th-nuernberg.de>
2021-01-06 14:52:21 +0100

    Made weapon more abstract and hopefully more fertile

--- those-bright-lights/Components/Sprites/Weapons/Weapon.cs
+++ those-bright-lights/Components/Sprites/Weapons/Weapon.cs
@@ -1,44 +1,148 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Runtime.CompilerServices;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using SE_Praktikum.Components.Sprites.Bullets;
+using SE_Praktikum.Services;
+using SE_Praktikum.Extensions;
 
 namespace SE_Praktikum.Components.Sprites.Weapons
 {
     public abstract class Weapon
     {
-        protected SoundEffect _shoot;
-        
-        /// <summary>
-        /// Milliseconds
-        /// </summary>
-        protected int CoolDown;
-        
-        /// <summary>
-        /// Milliseconds
-        /// </summary>
-        protected int ElapsedTimeSinceLastShoot;
+        protected readonly SoundEffect _shotSoundEffect;
 
-        public bool CanShoot => ElapsedTimeSinceLastShoot > CoolDown;
+        protected readonly CooldownAbility _shotAbility;
+        private float _rotation;
 
-        public Weapon(SoundEffect shoot)
+        // #############################################################################################################
+        // Constructor
+        // #############################################################################################################
+        /// <summary>
+        /// Base class for all weapons.
+        /// It implements the base mechanism for bullet creation, bullet firing and shot cooldown
+        /// </summary>
+        /// <param name="Parent"></param>
+        /// <param name="shotSoundEffect"></param>
+        /// <param name="nameTag">Name of the gun</param>
+        /// <param name="shotCoolDown">in milliseconds</param>
+        public Weapon(Actor Parent, SoundEffect shotSoundEffect, string nameTag, int shotCoolDown = 10)
         {
-            _shoot = shoot;
+            _shotAbility = new CooldownAbility(shotCoolDown, FireAbility);
+            _shotSoundEffect = shotSoundEffect;
+            NameTag = nameTag;
         }
-        public abstract Bullet GetBullet(Vector2 velocitySpaceship, Vector2 positionSpaceship,float Rotation, Actor parent);
 
+        // #############################################################################################################
+        // Events
+        // #############################################################################################################
+        public event EventHandler<EmitBulletEventArgs> OnEmitBullet;
+        
+        /// <summary>
+        /// Informs the subscriber about the current state of the shotCooldown.
+        /// the value will be between 0 and 1 and can be read as percent/100
+        /// </summary>
+        public event EventHandler<CooldownAbility.CooldownProgressArgs> OnShotCooldownProgressUpdate
+        {
+            add => _shotAbility.OnCoolDownUpdate += value;
+            remove => _shotAbility.OnCoolDownUpdate -= value;
+        }
+        
+        /// <summary>
+        /// will be triggered when the weapon is able to fire again
+        /// </summary>
+        public event EventHandler OnShotAvailable
+        {
+            add => _shotAbility.OnCoolAbilityAvailable += value;
+            remove => _shotAbility.OnCoolAbilityAvailable -= value;
+        }
+        
+        // #############################################################################################################
+        // Properties
+        // #############################################################################################################
+        public bool CanShoot => _shotAbility.AbilityAvailable;
+
+        /// <summary>
+        /// bullet rotation
+        /// </summary>
+        public float Rotation
+        {
+            get => _rotation;
+            set => _rotation = MathExtensions.Modulo2PiPositive(value);
+        }
+        
+        /// <summary>
+        /// bullet spawn point
+        /// </summary>
+        public Vector2 Position
+        {
+            get;
+            set;
+        }
+        
+        /// <summary>
+        /// Velocity of the moving system e.g. the spaceship
+        /// </summary>
+        public Vector2 Velocity
+        {
+            get;
+            set;
+        }
+
+        public string NameTag
+        {
+            get;
+            protected set;
+        }
+        
+        // #############################################################################################################
+        // public methods
+        // #############################################################################################################
         public void Update(GameTime gameTime)
         {
-            ElapsedTimeSinceLastShoot += gameTime.ElapsedGameTime.Milliseconds;
+            _shotAbility.Update(gameTime);
         }
 
-        public bool TryShoot()
+        public virtual void Fire()
         {
-            if (!CanShoot)
-                return false;
-            ElapsedTimeSinceLastShoot = 0;
-            return true;
+            _shotAbility.Fire();
         }
         
+        // #############################################################################################################
+        // private / protected methods
+        // #############################################################################################################
+        /// <summary>
+        /// use this to create the desired bullet
+        /// it will be called in the baseClasses FireAbility method which is triggered via the shotCooldownAbility
+        /// </summary>
+        /// <returns></returns>
+        protected abstract Bullet GetBullet();
+
+        /// <summary>
+        /// Method used to emit the actual bullet
+        /// override this to customize spray pattern, fire burst etc.
+        /// </summary>
+        protected virtual void FireAbility()
+        {
+            var e = new EmitBulletEventArgs() { Bullet = GetBullet()};
+            InvokeOnEmitBullet(e);
+            _shotSoundEffect?.Play();
+        }
+
+        // #############################################################################################################
+        // Invoker
+        // #############################################################################################################
+        protected virtual void InvokeOnEmitBullet(EmitBulletEventArgs e)
+        {
+            OnEmitBullet?.Invoke(this, e);
+        }
+
+        // #############################################################################################################
+        // Subclasses
+        // #############################################################################################################
+        public class EmitBulletEventArgs : EventArgs
+        {
+            public Bullet Bullet { get; set; }
+        }
     }
 }
