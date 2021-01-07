cac0ea7b12df2f9b891d2a87bf169b256ab55bf7
nachtmannan74843 <nachtmannan74843@th-nuernberg.de>
2021-01-07 02:56:43 +0100

    prepared methods for new infrastructure

--- those-bright-lights/Services/Factories/WeaponFactory.cs
+++ those-bright-lights/Services/Factories/WeaponFactory.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using SE_Praktikum.Components.Sprites;
+using SE_Praktikum.Components.Sprites.Bullets;
 using SE_Praktikum.Components.Sprites.Weapons;
 using SE_Praktikum.Models;
 
@@ -12,48 +14,56 @@ namespace SE_Praktikum.Services.Factories
 {
     public class WeaponFactory
     {
+        private readonly ContentManager _contentManager;
         private AnimationHandlerFactory _animationHandlerFactory;
         private readonly ParticleFactory _particleFactory;
         private readonly TileSetFactory _tileSetFactory;
+        private readonly BulletFactory _bulletFactory;
 
-        public WeaponFactory(AnimationHandlerFactory animationHandlerFactory, ParticleFactory particleFactory, TileSetFactory tileSetFactory)
+        public WeaponFactory(ContentManager contentManager,
+                             AnimationHandlerFactory animationHandlerFactory,
+                             ParticleFactory particleFactory,
+                             TileSetFactory tileSetFactory,
+                             BulletFactory bulletFactory)
         {
+            _contentManager = contentManager;
             _animationHandlerFactory = animationHandlerFactory;
             _particleFactory = particleFactory;
             _tileSetFactory = tileSetFactory;
+            _bulletFactory = bulletFactory;
         }
 
-        public MissileLauncher GetMissileLauncher(ContentManager contentManager)
+        public SingleShotWeapon GetMissileLauncher(Actor owner, int clipSize = 4, int clips = 10, int shotCooldown = 10, int reloadTime = 1000)
         {
-            var textureTileSet = _tileSetFactory.GetInstance(@".\Content\MetaData\TileSets\missile.json",0);
-            //Texture2D propulsion = contentManager.Load<Texture2D>("Artwork/projectiles/missile_propulsion_15_15");
-            //TileSet propulsionTileSet = new TileSet(propulsion,15,15, null);
-            var propulsionTileSet = _tileSetFactory.GetInstance(@".\Content\MetaData\TileSets\missile_propulsion_15_15.json", 0);
-            SoundEffect flightEffect = null; //contentManager.Load<SoundEffect>("Audio/Sound_Effects/Airborne/Flight_plane_c");
-            SoundEffect impactSound = contentManager.Load<SoundEffect>("Audio/Sound_Effects/Collusion/Big_Explo");
-            var m = new MissileLauncher(_animationHandlerFactory,textureTileSet, propulsionTileSet,
-                _particleFactory, null, flightEffect, impactSound);
+            // TODO: create and load missing sound effects
+            var m = new SingleShotWeapon(owner,
+                                         null, 
+                                         null, 
+                                         null,
+                                         null,
+                                         "Missile Launcher",
+                                         clipSize,
+                                         clips,
+                                         () => _bulletFactory.GetMissile(owner),
+                                         shotCoolDown: shotCooldown,
+                                         reloadTime: reloadTime);
             return m;
         }
 
-        public Lasergun GetLasergun(ContentManager contentManager)
+        public SingleShotWeapon GetLasergun(Actor owner)
         {
             var laserTileSet = _tileSetFactory.GetInstance(@".\Content\MetaData\TileSets\laser.json",0);
-            SoundEffect soundEffect = contentManager.Load<SoundEffect>("Audio/Sound_Effects/Shot/Laser_Short");
-            SoundEffect flightEffect = contentManager.Load<SoundEffect>("Audio/Sound_Effects/Airborne/Wobble_test");
-            SoundEffect impactSound = contentManager.Load<SoundEffect>("Audio/Sound_Effects/Collusion/Clink");
-            var l = new Lasergun(_animationHandlerFactory, laserTileSet, _particleFactory, soundEffect, flightEffect, impactSound);
-            return l;
+            SoundEffect soundEffect = _contentManager.Load<SoundEffect>("Audio/Sound_Effects/Shot/Laser_Short");
+            // var l = new Lasergun(_animationHandlerFactory, laserTileSet, _particleFactory, soundEffect, flightEffect, impactSound);
+            return null;
         }
 
-        public Lasergun EnemyGetLasergun(ContentManager contentManager)
+        public SingleShotWeapon EnemyGetLasergun(Actor owner)
         {
             var enemylaserTileSet = _tileSetFactory.GetInstance(@".\Content\MetaData\TileSets\enemylaser.json", 0);
-            SoundEffect soundEffect = contentManager.Load<SoundEffect>("Audio/Sound_Effects/Shot/Laser_Short");
-            SoundEffect flightEffect = contentManager.Load<SoundEffect>("Audio/Sound_Effects/Airborne/Wobble_test");
-            SoundEffect impactSound = contentManager.Load<SoundEffect>("Audio/Sound_Effects/Collusion/Clink");
-            var k = new Lasergun(_animationHandlerFactory, enemylaserTileSet, _particleFactory, soundEffect, flightEffect, impactSound);
-            return k;
+            SoundEffect soundEffect = _contentManager.Load<SoundEffect>("Audio/Sound_Effects/Shot/Laser_Short");
+            // var k = new Lasergun(_animationHandlerFactory, enemylaserTileSet, _particleFactory, soundEffect, flightEffect, impactSound);
+            return null;
         }
     }
 }
