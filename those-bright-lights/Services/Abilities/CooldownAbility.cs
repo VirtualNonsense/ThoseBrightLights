c158a8885a21c0580a4c6cb602e6ac16e05a589b
nachtmannan74843 <nachtmannan74843@th-nuernberg.de>
2021-01-08 00:41:00 +0100

    moved parts in parent class because it would be cool to have a casttimeability and i'd to avoid code duplicates

--- those-bright-lights/Services/Abilities/CooldownAbility.cs
+++ those-bright-lights/Services/Abilities/CooldownAbility.cs
@@ -3,86 +3,42 @@ using Microsoft.Xna.Framework;
 
 namespace SE_Praktikum.Services.Abilities
 {
-    public class CooldownAbility
+    public class CooldownAbility : TimeControlledAbility
     {
-        private int _elapsedTime;
-        private int _cooldown;
 
-        public event EventHandler<CooldownProgressArgs> OnCoolDownUpdate;
-        public event EventHandler OnCoolAbilityAvailable;
-
-        public CooldownAbility(int cooldown, Action ability)
+        
+        // #############################################################################################################
+        // Constructor
+        // #############################################################################################################
+        public CooldownAbility(int cooldown, Action ability) : base(cooldown, ability)
         {
             _elapsedTime = cooldown;
-            _cooldown = cooldown;
-            _ability = ability;
         }
 
-        private readonly Action _ability;
-
-        public int Cooldown
+        
+        // #############################################################################################################
+        // Properties
+        // #############################################################################################################
+        
+        public bool AbilityAvailable => TargetTimeReached;
+        
+        // #############################################################################################################
+        // Public Methods
+        // #############################################################################################################
+        public override void Update(GameTime gameTime)
         {
-            get => _cooldown;
-            set
-            {
-                if (value < 0) return;
-                _cooldown = value;
-            }
-        }
-
-        public bool AbilityAvailable => _cooldown <= _elapsedTime;
-
-        public float CoolDownProgress
-        {
-            get
-            {
-                var progress = _elapsedTime / (float) _cooldown;
-                return progress > 1 ? 1 : progress;
-            }
-        }
-
-        public void Update(GameTime gameTime)
-        {
-            if (AbilityAvailable) return;
-            _elapsedTime += (int)gameTime.ElapsedGameTime.TotalMilliseconds;
+            if(TargetTimeReached) return;
+            base.Update(gameTime);
             InvokeCoolDownUpdate();
             if(AbilityAvailable)
                 InvokeOnCoolAbilityAvailable();
         }
 
-        public void Fire()
+        public override void Fire()
         {
             if (!AbilityAvailable) return;
             _elapsedTime = 0;
             _ability();
-        }
-
-        /// <summary>
-        /// Update the subscriber about the cooldown progress
-        /// Use values between 0 and 1
-        /// </summary>
-        protected virtual void InvokeCoolDownUpdate()
-        {
-            var e = new CooldownProgressArgs(CoolDownProgress);
-            OnCoolDownUpdate?.Invoke(this, e);
-        }
-        protected virtual void InvokeOnCoolAbilityAvailable()
-        {
-            OnCoolAbilityAvailable?.Invoke(this, EventArgs.Empty);
-        }
-        
-        public class CooldownProgressArgs : EventArgs
-        {
-            /// <summary>
-            /// Value should be between 0 and 1.
-            /// 1 means the cooldown is done and the ability is available.
-            /// </summary>
-            public float Progress { get; set; }
-
-            public CooldownProgressArgs(float progress)
-            {
-                Progress = progress;
-            }
         }
 
     }
