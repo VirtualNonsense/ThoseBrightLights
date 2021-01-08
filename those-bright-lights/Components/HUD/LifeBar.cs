57097720428dc8ce68ad931970b0a4e1e24442e9
weinertch74668 <weinertch74668@th-nuernberg.de>
2021-01-08 18:01:03 +0100

    Minimum heart value

--- those-bright-lights/Components/HUD/LifeBar.cs
+++ those-bright-lights/Components/HUD/LifeBar.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SE_Praktikum.Services;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +9,32 @@ namespace SE_Praktikum.Components.HUD
 {
     public class LifeBar : HUDItem
     {
+        public int HealthPerHeart { get; set; }
+
+        public LifeBar(HUD parent, List<AnimationHandler> handler) : base(parent, handler)
+        {
+            HealthPerHeart = 5;
+            parent.Player.OnHealthChanged += Player_OnHealthChanged; 
+        }
+
+        private void Player_OnHealthChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        private void UpdateHealthBar()
+        {
+
+        }
     }
 }
