8557ff50b2f8f0b4c46ad6fec8cb5cb941192e3c
weinertch74668 <weinertch74668@th-nuernberg.de>
2021-01-08 21:22:45 +0100

    Placing first heart

--- those-bright-lights/Services/Factories/HUDFactory.cs
+++ those-bright-lights/Services/Factories/HUDFactory.cs
@@ -1,5 +1,7 @@
-﻿using SE_Praktikum.Components.HUD;
+﻿using Microsoft.Xna.Framework;
+using SE_Praktikum.Components.HUD;
 using SE_Praktikum.Components.Sprites.Actors.Spaceships;
+using SE_Praktikum.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,17 +11,25 @@ namespace SE_Praktikum.Services.Factories
     public class HUDFactory
     {
         private readonly HUDItemFactory hUDItemFactory;
+        private readonly IScreen screen;
 
-        public HUDFactory(HUDItemFactory hUDItemFactory)
+        public HUDFactory(HUDItemFactory hUDItemFactory, IScreen screen)
         {
             this.hUDItemFactory = hUDItemFactory;
+            this.screen = screen;
         }
 
         public HUD GetInstance(Player player)
         {
             var hud = new HUD(player);
+            var lifebar = hUDItemFactory.GetLifeBar(hud);
+            var width = screen.Camera.GetPerspectiveScreenWidth(player.Layer);
+            var height = screen.Camera.GetPerspectiveScreenHeight(player.Layer);
+           
 
-            hud.AddHUDItem(hUDItemFactory.GetLifeBar(hud));
+            lifebar.Position = new Vector2(-width / 2f, -height / 2f);
+
+            hud.AddHUDItem(lifebar);
 
             return hud;
         }
