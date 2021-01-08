9d9668930eb7d687018856311060cca93922c423
weinertch74668 <weinertch74668@th-nuernberg.de>
2021-01-08 17:11:23 +0100

    Added constructor

--- those-bright-lights/Components/HUD/HUD.cs
+++ those-bright-lights/Components/HUD/HUD.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using SE_Praktikum.Components.Sprites.Actors.Spaceships;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,8 +9,19 @@ namespace SE_Praktikum.Components.HUD
 {
     public class HUD : IComponent
     {
+        private readonly Player _player;
+        private readonly List<HUDItem> _hUDItems;
+
         public Vector2 Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public bool IsRemoveAble { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+       
+
+        public HUD(Player player, List<HUDItem> hUDItems)
+        {
+            this._player = player;
+            this._hUDItems = hUDItems;
+        }
 
         public void Draw(SpriteBatch spriteBatch)
         {
