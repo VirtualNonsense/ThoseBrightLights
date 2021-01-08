a719fb709e6bf216304649fc3e90c03c42d93112
weinertch74668 <weinertch74668@th-nuernberg.de>
2021-01-08 16:57:34 +0100

    Added constructor

--- those-bright-lights/Components/HUD/HUDItem.cs
+++ those-bright-lights/Components/HUD/HUDItem.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using SE_Praktikum.Models;
+using SE_Praktikum.Services;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,14 +13,28 @@ namespace SE_Praktikum.Components.HUD
         public Vector2 Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public bool IsRemoveAble { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
+        public List<AnimationHandler> _handler;
+        public HUD _parent;
+
+        public HUDItem(HUD parent, List<AnimationHandler> handler)
+        {
+            _parent = parent;
+            _handler = handler;
+        }
+
         public void Draw(SpriteBatch spriteBatch)
         {
-            throw new NotImplementedException();
+            foreach (var item in _handler)
+            {
+                item.Draw(spriteBatch);
+            }
         }
 
         public void Update(GameTime gameTime)
         {
             throw new NotImplementedException();
         }
+
+      
     }
 }
