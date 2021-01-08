77f0388a052a1769338d874ffede97282a7535a0
weinertch74668 <weinertch74668@th-nuernberg.de>
2021-01-08 16:33:53 +0100

    Introduced idea of HUD and HUDItems

---
+++ those-bright-lights/Components/HUD/HUD.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SE_Praktikum.Components.HUD
+{
+    public class HUD : IComponent
+    {
+        public Vector2 Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool IsRemoveAble { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
