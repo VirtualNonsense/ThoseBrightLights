1b2b9d36ebb01b5524ce95199e3fc3cae7320519
weinertch74668 <weinertch74668@th-nuernberg.de>
2021-01-08 19:21:43 +0100

    Added constructor

--- those-bright-lights/Services/Factories/HUDItemFactory.cs
+++ those-bright-lights/Services/Factories/HUDItemFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SE_Praktikum.Components.HUD;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,16 @@ namespace SE_Praktikum.Services.Factories
 {
     public class HUDItemFactory
     {
+        private readonly TileSetFactory tileSetFactory;
+        private readonly AnimationHandlerFactory animationHandlerFactory;
+
+        public HUDItemFactory(TileSetFactory tileSetFactory, AnimationHandlerFactory animationHandlerFactory)
+        {
+            this.tileSetFactory = tileSetFactory;
+            this.animationHandlerFactory = animationHandlerFactory;
+        }
+
+
+
     }
 }
