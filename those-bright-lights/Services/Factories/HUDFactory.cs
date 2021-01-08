52e051b3ab749dfd02b7dc2e41b2a39bff378af8
weinertch74668 <weinertch74668@th-nuernberg.de>
2021-01-08 19:56:57 +0100

    Added functionality to Hudfactory

--- those-bright-lights/Services/Factories/HUDFactory.cs
+++ those-bright-lights/Services/Factories/HUDFactory.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SE_Praktikum.Components.HUD;
+using SE_Praktikum.Components.Sprites.Actors.Spaceships;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +8,20 @@ namespace SE_Praktikum.Services.Factories
 {
     public class HUDFactory
     {
+        private readonly HUDItemFactory hUDItemFactory;
+
+        public HUDFactory(HUDItemFactory hUDItemFactory)
+        {
+            this.hUDItemFactory = hUDItemFactory;
+        }
+
+        public HUD GetInstance(Player player)
+        {
+            var hud = new HUD(player);
+
+            hud.AddHUDItem(hUDItemFactory.GetLifeBar(hud));
+
+            return hud;
+        }
     }
 }
