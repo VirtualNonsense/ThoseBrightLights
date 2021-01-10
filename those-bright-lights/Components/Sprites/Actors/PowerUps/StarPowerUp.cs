854134cb1b53f11002c077830ca515e769f63073
Jonathan Wenk <wenkjo75592@th-nuernberg.de>
2021-01-10 21:55:29 +0100

    StarPowerUp class added

---
+++ those-bright-lights/Components/Sprites/Actors/PowerUps/StarPowerUp.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework.Audio;
+using SE_Praktikum.Services;
+
+
+namespace SE_Praktikum.Components.Sprites.Actors.PowerUps
+{
+    public class StarPowerUp : PowerUp
+    {
+        
+        public StarPowerUp(AnimationHandler animationHandler, float health = 0.01f, SoundEffect soundEffect = null) : base(animationHandler, health, soundEffect)
+        {
+            
+        }
+    }
+}
