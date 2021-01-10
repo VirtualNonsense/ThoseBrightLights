758c66b23e8916ecdc4f4c825ba497235aa24a5b
Jonathan Wenk <wenkjo75592@th-nuernberg.de>
2021-01-10 19:00:14 +0100

    FullHealthPowerUp class

---
+++ those-bright-lights/Components/Sprites/Actors/PowerUps/FullHealthPowerUp.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework.Audio;
+using SE_Praktikum.Services;
+
+namespace SE_Praktikum.Components.Sprites.Actors.PowerUps
+{
+    public class FullHealthPowerUp : PowerUp
+    {
+        public readonly float HealthBonus;
+        public FullHealthPowerUp(AnimationHandler animationHandler, float healthbonus, float health = 0.01f, SoundEffect soundEffect = null) : base(animationHandler, health, soundEffect)
+        {
+            HealthBonus = healthbonus;
+        }
+    }
+}
