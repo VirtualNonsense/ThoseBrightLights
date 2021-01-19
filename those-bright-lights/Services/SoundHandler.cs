b6f75645bb4be82f858befd6990662f2579332d7
weinertch74668 <weinertch74668@th-nuernberg.de>
2021-01-19 12:56:38 +0100

    Comments for the handlers

--- those-bright-lights/Services/SoundHandler.cs
+++ those-bright-lights/Services/SoundHandler.cs
@@ -7,8 +7,10 @@ namespace SE_Praktikum.Services
 {
     public class SoundHandler<T> where T : Enum
     {
+        // Class for determining which soundeffect is used more easily
         private Dictionary<T, SoundEffect> _soundEffect;
 
+        // If there is a Soundeffect then give it back
         public SoundEffect Get(T key)
         {
             if (_soundEffect.ContainsKey(key))
@@ -18,16 +20,19 @@ namespace SE_Praktikum.Services
             return null;
         }
 
+        // Constructor
         public SoundHandler()
         {
             _soundEffect = new Dictionary<T, SoundEffect>(); 
         }
 
+        // Soundeffect created map them
         public void Add(T key, SoundEffect soundEffect)
         {
             _soundEffect.Add(key, soundEffect);
         }
 
+        // Play the sound
         public void Play(T key)
         {
             _soundEffect[key].CreateInstance().Play();
