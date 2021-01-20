22dfafe0e248eddb8e38889e3ee00df3275cdd6c
nachtmannan74843 <nachtmannan74843@th-nuernberg.de>
2021-01-20 14:26:55 +0100

    comments and refactoring

--- those-bright-lights/Components/IComponent.cs
+++ those-bright-lights/Components/IComponent.cs
@@ -4,16 +4,32 @@ using Microsoft.Xna.Framework.Graphics;
 namespace SE_Praktikum.Components
 {
     /// <summary>
-    /// Derive from this class if you 
+    /// This is the Interface for everything that is drawable/updatable
+    /// use this to implement anything that needs to be drawn / updated to simplify this process
     /// </summary>
     public interface IComponent
     {
+        /// <summary>
+        /// Position of the objects origin in world space
+        /// </summary>
         Vector2 Position { get; set; }
         
+        /// <summary>
+        /// Draw the object
+        /// </summary>
+        /// <param name="spriteBatch">the "canvas the object will be drawn on"</param>
         void Draw(SpriteBatch spriteBatch);
 
+        /// <summary>
+        /// Updates the object
+        /// e.g should the object be moved? etc.
+        /// </summary>
+        /// <param name="gameTime"></param>
         void Update(GameTime gameTime);
 
+        /// <summary>
+        /// Is true when the item needs to be removed
+        /// </summary>
         bool IsRemoveAble { get; set; }
     }
 }
