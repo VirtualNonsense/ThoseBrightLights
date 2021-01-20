f2330d0d05f7aed5e92d869a5b1fae70bd8cc143
pillhoferjo74678 <pillhoferjo74678@th-nuernberg.de>
2021-01-21 00:31:18 +0100

    comments and refactoring

--- those-bright-lights/Components/Sprites/Actors/PowerUps/BonusClipPowerUp.cs
+++ those-bright-lights/Components/Sprites/Actors/PowerUps/BonusClipPowerUp.cs
@@ -7,6 +7,7 @@ namespace SE_Praktikum.Components.Sprites.Actors.PowerUps
     public class BonusClipPowerUp : PowerUp
     {
         private int _bonusClips;
+        
         public BonusClipPowerUp(AnimationHandler animationHandler, int bonusClips ,float health = 0.01f, SoundEffect soundEffect = null) : base(animationHandler, health, soundEffect)
         {
             _bonusClips = bonusClips;
