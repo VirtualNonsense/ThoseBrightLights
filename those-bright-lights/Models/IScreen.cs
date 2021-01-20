f865db9f60785a1acbcc164ed72f4881d611de73
nachtmannan74843 <nachtmannan74843@th-nuernberg.de>
2021-01-20 17:55:07 +0100

    comments and refactoring

--- those-bright-lights/Models/IScreen.cs
+++ those-bright-lights/Models/IScreen.cs
@@ -2,6 +2,11 @@
 
 namespace SE_Praktikum.Models
 {
+    
+    /// <summary>
+    /// contains everything to know about the screen size
+    /// for screen width on a specific layer use camera
+    /// </summary>
     public interface IScreen
     {
         int ScreenHeight { get; }
