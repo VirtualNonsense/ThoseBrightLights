df2a7931a11b0167749770393217485c6246f49a
nachtmannan74843 <nachtmannan74843@th-nuernberg.de>
2021-01-20 14:15:29 +0100

    comments and refactoring

--- those-bright-lights/Components/Sprites/StatusEffectParticle.cs
+++ those-bright-lights/Components/Sprites/StatusEffectParticle.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
 using NLog;
-using SE_Praktikum.Models;
 using SE_Praktikum.Services;
 using SE_Praktikum.Services.Abilities;
 using SE_Praktikum.Services.Factories;
@@ -11,9 +10,16 @@ namespace SE_Praktikum.Components.Sprites
     {
         private Logger _logger;
         private CastTimeAbility _destructionTimer;
-        public StatusEffectParticle(AnimationHandler animationHandler, IScreen Parent, int destructionTime) : base(animationHandler)
+
+        /// <summary>
+        /// Particle that is meant for status effects invincibility for example 
+        /// </summary>
+        /// <param name="animationHandler"></param>
+        /// <param name="destructionTime"></param>
+        public StatusEffectParticle(AnimationHandler animationHandler, int destructionTime) : base(animationHandler)
         {
             _logger = LogManager.GetCurrentClassLogger();
+            // self destruct when time is up
             _destructionTimer = new CastTimeAbility(destructionTime, () =>
             {
                 IsRemoveAble = true;
@@ -23,14 +29,17 @@ namespace SE_Praktikum.Components.Sprites
 
         public override void Update(GameTime gameTime)
         {
+            //updating timer
             _destructionTimer.Update(gameTime);
+            
+            // scale decreases when time progresses
             Scale = 1 - _destructionTimer.CoolDownProgress * Scale;
             base.Update(gameTime);
         }
 
         public override Particle Clone()
         {
-            throw new System.NotImplementedException();
+            return MemberwiseClone() as Particle;
         }
     }
 }
