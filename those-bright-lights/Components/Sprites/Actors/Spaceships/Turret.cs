2fc9a9903fa40318a69b8bdff0ac12e2b7915a19
pillhoferjo74678 <pillhoferjo74678@th-nuernberg.de>
2021-01-21 10:21:32 +0100

    refactoring and comments

--- those-bright-lights/Components/Sprites/Actors/Spaceships/Turret.cs
+++ those-bright-lights/Components/Sprites/Actors/Spaceships/Turret.cs
@@ -10,19 +10,37 @@ namespace SE_Praktikum.Components.Sprites.Actors.Spaceships
     public class Turret : EnemyWithViewbox
     {
         private Logger _logger;
+        
+        // #############################################################################################################
+        // Constructor
+        // #############################################################################################################
+        /// <summary>
+        /// Class for the turret enemy
+        /// </summary>
+        /// <param name="animationHandler"></param>
+        /// <param name="viewbox"></param>
+        /// <param name="maxSpeed"></param>
+        /// <param name="acceleration"></param>
+        /// <param name="health"></param>
+        /// <param name="maxHealth"></param>
+        /// <param name="impactSound"></param>
         public Turret(AnimationHandler animationHandler,
                       Polygon viewbox,
                       float maxSpeed = 3,
                       float acceleration = 0,
                       float health = 50,
                       float maxHealth = 50,
-                      SoundEffect impactSound = null) : base(animationHandler, viewbox, maxSpeed, acceleration, health: health, maxHealth: maxHealth)
+                      SoundEffect impactSound = null) 
+            : base(animationHandler, viewbox, maxSpeed, acceleration, health: health, maxHealth: maxHealth)
         {
             _impactSound = impactSound;
             _logger = LogManager.GetCurrentClassLogger();
             RotateWeapon = true;
         }
 
+        // #############################################################################################################
+        // public Methods
+        // #############################################################################################################
         public override void Update(GameTime gameTime)
         {
             Shoot.Update(gameTime);
